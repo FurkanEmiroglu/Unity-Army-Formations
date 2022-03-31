9eb39f254c3be2c11d78746c89845f05c930c1e4
Furkan Emiroglu <emiroglu.furkan@gmail.com>
2022-03-31 04:17:22 +0300

    Initialize

---
+++ Assets/dieoncollision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnoncollision : MonoBehaviour
+{
+    public GameObject leader;
+    [SerializeField] RadialFormation formation;
+
+    private void Start()
+    {
+        formation = leader.GetComponent<RadialFormation>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        formation._amount--;
+    }
+}
